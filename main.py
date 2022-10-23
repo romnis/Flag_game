39d61586ffd018ca3b49e9bf6a6c288e1e67e4c6
Rom Nissim <rom.nissim@gmail.com>
2022-10-23 11:02:26 +0300

    try

---
+++ main.py
@@ -0,0 +1,15 @@
+
+state = {
+    "original_arrow": Screen.create_arrow(consts.ARROW_IMG),
+    "rotated_arrow": None,
+    "is_bubble_fired": False,
+    "bubbles_popping": [],
+    "turns_left_to_add_row": consts.NUM_OF_TURNS_TO_ADD_ROW,
+    "is_window_open": True,
+    "state": consts.RUNNING_STATE,
+    "bullet_bubble": None,
+    "bubble_direction": None,
+    "mouse_angle": None,
+    "score": 0
+    #ff
+}
